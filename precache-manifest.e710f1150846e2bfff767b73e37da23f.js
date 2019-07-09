4256a72cca11593a6d3d0fd47904754d1e88e97c
Tibo <tiborkeri@icloud.com>
2019-07-09 22:07:36 +0100

    Updates

---
+++ precache-manifest.e710f1150846e2bfff767b73e37da23f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "e8fa102996fed6a1d1e4",
+    "url": "/linklaters-test/static/css/main.748a89ee.chunk.css"
+  },
+  {
+    "revision": "e8fa102996fed6a1d1e4",
+    "url": "/linklaters-test/static/js/main.e8fa1029.chunk.js"
+  },
+  {
+    "revision": "b867b74fb4dfa53281f5",
+    "url": "/linklaters-test/static/js/1.b867b74f.chunk.js"
+  },
+  {
+    "revision": "f25be62894d14047d653",
+    "url": "/linklaters-test/static/js/runtime~main.f25be628.js"
+  },
+  {
+    "revision": "bf2693cf6689d0f64040c9f647356891",
+    "url": "/linklaters-test/index.html"
+  }
+];
