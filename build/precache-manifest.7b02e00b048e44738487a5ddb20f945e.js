dd9a3432b3a0a06b06cc26851d864be991ba0e6a
Tibo <tiborkeri@icloud.com>
2019-07-09 21:55:08 +0100

    2nd attempt

---
+++ build/precache-manifest.7b02e00b048e44738487a5ddb20f945e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "d6a3f9b5c2ec71d19cae",
+    "url": "/static/css/main.49a2edd4.chunk.css"
+  },
+  {
+    "revision": "d6a3f9b5c2ec71d19cae",
+    "url": "/static/js/main.d6a3f9b5.chunk.js"
+  },
+  {
+    "revision": "b867b74fb4dfa53281f5",
+    "url": "/static/js/1.b867b74f.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "2fb51991a5f230548a1dc329fa302016",
+    "url": "/index.html"
+  }
+];
